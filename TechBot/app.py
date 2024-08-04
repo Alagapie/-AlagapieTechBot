2e064b6f9e6caed93dea3038fa0bb517ba187a50
Alagapie <abdulbasitsuleiman660@gmail.com>
2024-08-04 03:24:06 +0100

    Update app.py

--- TechBot/app.py
+++ TechBot/app.py
@@ -99,11 +99,11 @@ if selected == 'Alagapie ChatMate':
     prompt=st.chat_input("Ask Alagapie chatbot anything ")
     if prompt:
       st.chat_message('user').markdown(prompt)
-      placeholder = st.empty()
-      with placeholder:
-        with st.spinner("Generating response..."):
+      
+      
+      with st.spinner("Generating response..."):
             response = st.session_state.chat_session.send_message(prompt)
-            placeholder.empty()
+           
 
       with st.chat_message("assistant"):
         st.markdown(response.text)
